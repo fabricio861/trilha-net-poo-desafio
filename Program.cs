dd0dffb761e564e24c7b2ec38ab4e21454b12ee9
fabricio861 <fabriciomansano340@gmail.com>
2025-11-06 23:06:27 -0300

    6/11

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("123456", "Nokia Tijolão", "111111", 1);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Smartphone iphone = new Iphone("40028922", "Iphone 17", "222222", 2);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+    }
+
+}
